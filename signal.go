b0d030f440baf9901982edd9913e192badfa3edf
Duy Nguyen <tapvanvn@gmail.com>
2021-06-24 18:43:18 +0700

    update

--- signal.go
+++ signal.go
@@ -2,5 +2,5 @@ package godashboard
 
 type Signal struct {
 	ItemName string           `json:"item_name" bson:"item_name"`
-	Params   map[string]Param `json:"signal" bson:"signal"`
+	Params   map[string]Param `json:"params" bson:"params"`
 }
